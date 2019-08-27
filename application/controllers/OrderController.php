48b87d86775e3503976e89c5a2e457e0d20ac24b
farhan alfiansyah <farhanalfiansyah30@gmail.com>
2019-08-27 19:46:17 +0700

    Update new

--- application/controllers/OrderController.php
+++ application/controllers/OrderController.php
@@ -2,9 +2,17 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class OrderController extends CI_Controller {
+        public function __construct(){
+                parent::__construct();
+                $this->load->model('Penjualan');
+                $this->load->model('DetailPenjualan');
+            }
 
-        public function index()
+        public function createOrder()
         {
-                echo 'Hello World!';
+                $DataPenjualan = json_decode($this->input->raw_input_stream,true);
+                if($DataPenjualan){
+                        
+                }
         }
 }
